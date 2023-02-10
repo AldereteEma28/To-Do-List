d9c56c5bf86fe407f1e1715f89301741a6cfc28a
AldereteEma28 <88401301+AldereteEma28@users.noreply.github.com>
2023-02-10 09:57:02 -0300

    cambios en la url de archivos estaticos

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -104,10 +104,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'meida')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR),'TO_DO/static'),
+    )
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
 MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
