88978ef1422ff6189f449ac4769749baaf936664
AldereteEma28 <aldereteema28@gmail.com>
2023-02-19 19:21:54 -0300

    cambio en los modelos

--- to_do/task/models.py
+++ to_do/task/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Task(models.Model):
     task_name = models.CharField(max_length=200)
-    task_desc = models.TextField()
+    task_desc = models.TextField(blank=True)
     task_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
